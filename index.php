0e9ff0536a4030d9ded09a2f5a890ba72207e5dd
Clément <clementgaubert44@gmail.com>
2024-11-15 15:02:19 +0100

    includes vers views + creation index redirection

--- index.php
+++ index.php
@@ -1,28 +1,4 @@
-<?php 
-include('includes/header.php');
-include('utils/db_connection.php');
-
-// Préparation et exécution de la requête SQL
-//$stmt = $???->prepare("SELECT titre, description FROM idees");
-//$stmt->execute();
-//$result = $stmt->get_result();
-
-echo "<h1>Bienvenue</h1>";
-echo "<h2>Liste des idées</h2>";
-
-// Affichage des idées
-if ($result->num_rows > 0) {
-    echo "<ul>";
-    while ($row = $result->fetch_assoc()) {
-        echo "<li>";
-        echo "<strong>" . htmlspecialchars($row['titre']) . "</strong><br>";
-        echo htmlspecialchars($row['description']);
-        echo "</li>";
-    }
-    echo "</ul>";
-} else {
-    echo "<p>Aucune idée trouvée.</p>";
-}
-
-include('includes/footer.php'); 
-?>
+<?php
+// Redirige vers accueil.php
+header("Location: views/accueil.php");
+exit();
--- views/header.php
+++ views/header.php
@@ -11,7 +11,7 @@ if (session_status() === PHP_SESSION_NONE) {
 <head>
     <meta charset="UTF-8">
     <title>Boite à idées</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style.css">
 </head>
 
 <body>
