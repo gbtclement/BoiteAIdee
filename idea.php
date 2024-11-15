326e1784953474134a46748992520aaf9a6cf3b7
MateoLgr <mateolegrand76@gmail.com>
2024-11-15 15:00:26 +0100

    debut du dev idea
    formulaire fait

---
+++ idea.php
@@ -0,0 +1,51 @@
+<?php include('../includes/header.php'); ?>
+<?php
+$titre = null;
+$message = null;
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $titre = filter_input(INPUT_POST, "titre");
+    $message = filter_input(INPUT_POST, "message");
+}
+?>
+<!-- Contenu principal de la page d'accueil ici -->
+
+<!-- <h1>Création de l'idée</h1> -->
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- utf-8 sur la page -->
+        <meta charset="utf-8">
+
+        <!-- Titre de la page -->
+        <title> Créer mon idée</title>
+
+
+    </head>
+
+    <body>
+
+        <!-- formulaire en POST pour recupérer les données -->
+        <form action="" method="POST" class="formulaire">
+
+            <div class="formulaire">
+                <label for="titre">Votre titre: </label>
+                <input type="text" name="titre" id="titre" required />
+            </div>
+
+            <div class="formulaire">
+                <label for="message">Votre message :</label>
+                <!--Max length de 65 535 car le type text de mysql a cettee limite de charactères -->
+                <textarea cols="" rows="" name="message" maxlength="65530"></textarea>
+            </div>
+
+            <div class="formulaire">
+                <input type="submit" value="Envoyer !" />
+            </div>
+
+        </form>
+
+    </body>
+</html>
+
+<?php include('../includes/footer.php'); ?>
